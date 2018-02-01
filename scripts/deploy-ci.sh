371dd4e60fa6d28d2587658a0c9bc799046a9f5f
howdy39 <tatsuya.nakano@topgate.co.jp>
2018-02-01 16:18:58 +0900

    フォルダ構成を変更

--- scripts/deploy-ci.sh
+++ scripts/deploy-ci.sh
@@ -2,4 +2,4 @@
 
 echo $DEV_SERVICE_ACCOUNT_KEY > /tmp/secret.json
 gcloud auth activate-service-account $DEV_SERVICE_ACCOUNT_CLIENT_EMAIL --key-file /tmp/secret.json
-appcfg.py --application `gcloud config get-value project` --version=app-ci --oauth2_access_token `gcloud auth print-access-token` update ./app/app.yaml
+appcfg.py --application `gcloud config get-value project` --version=app-ci --oauth2_access_token `gcloud auth print-access-token` update ./api/src/app.yaml
