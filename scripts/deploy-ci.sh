f3084aa5ae903cd3733853749254375bbe36d3d7
howdy39 <tatsuya.nakano@topgate.co.jp>
2018-02-01 16:35:52 +0900

    no message

--- scripts/deploy-ci.sh
+++ scripts/deploy-ci.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo $DEV_SERVICE_ACCOUNT_KEY > /tmp/secret.json
-gcloud auth activate-service-account $DEV_SERVICE_ACCOUNT_CLIENT_EMAIL --key-file /tmp/secret.json
-appcfg.py --application `gcloud config get-value project` --version=app-ci --oauth2_access_token `gcloud auth print-access-token` update ./api/src/app.yaml
+echo $SERVICE_ACCOUNT_KEY > /tmp/secret.json
+gcloud auth activate-service-account $SERVICE_ACCOUNT_CLIENT_EMAIL --key-file /tmp/secret.json
+appcfg.py --application $PROJECT_ID --version=app-ci --oauth2_access_token `gcloud auth print-access-token` update ./api/src/app.yaml
